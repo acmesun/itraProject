be6d65bf4ea81575a79a0eef55c30bd233c6a625
alukyanets <>
2022-12-06 20:56:08 +0300

    Initial commit

---
+++ src/main/java/com/itransition/itraproject/ItraProjectApplication.java
@@ -0,0 +1,15 @@
+package com.itransition.itraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItraProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ItraProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itransition/itraproject/ItraProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.itransition.itraproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItraProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
